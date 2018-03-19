8cfd065a96b93228eb10a87f6a9966185849887a
Yeiko <nativ.yeiko@gmail.com>
2018-03-19 23:23:51 +0100

    Merge branch 'master' of https://github.com/NativYeiko/premi-re-ia

---
+++ ressources/ia/main.js
@@ -0,0 +1,6 @@
+// Ia Appelle
+var commands = {
+  'Salut Jaris': function() { alert('Salut Aymeric')},
+  'Comment vas tu': function(){ alert('Bien et toi')},
+  'Jaris ouvre la map': function() { };
+};
